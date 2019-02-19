8f3642deaf0fc00344b895b8b9d793d56f0f7fd4
TheBlockCode <jesussm_0001@hotmail.com>
2019-02-18 22:04:19 -0600

    copiaxd

---
+++ Tarea6/DBej6MVVM/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace DBej6MVVM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        public static DataGrid datag;
+        int id;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+ 
+        }
+
+        private void UpdateBTN_Click(object sender, RoutedEventArgs e)
+        {
+            int categoryId = (DataGrid1.SelectedItem as Categories).CategoryID;
+            WinUpdate Upage = new WinUpdate(categoryId);
+            Upage.ShowDialog();
+        }
+
+        private void LoadBTN_Click(object sender, RoutedEventArgs e)
+        {
+            DataGrid1.ItemsSource = db.Categories.ToList();
+            datag = DataGrid1;
+        }
+    }
+}
