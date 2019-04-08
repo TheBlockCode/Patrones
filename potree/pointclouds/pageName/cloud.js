430ea3f20d0997ff5e7021b471289b97752009da
TheBlockCode <jesussm_0001@hotmail.com>
2019-04-08 11:24:38 -0500

    tarea 2parcial

---
+++ potree/pointclouds/pageName/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 670844,
+    "boundingBox": {
+        "lx": -10.707018,
+        "ly": -6.013485,
+        "lz": 16.56442,
+        "ux": -6.175929999999999,
+        "uy": -1.4823969999999998,
+        "uz": 21.095508
+    },
+    "tightBoundingBox": {
+        "lx": -10.707018,
+        "ly": -6.013485,
+        "lz": 16.56442,
+        "ux": -8.958963,
+        "uy": -1.482397,
+        "uz": 19.542051
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 0.03924037143588066,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
